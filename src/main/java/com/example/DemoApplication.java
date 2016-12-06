e073e918b87cb6f9f2c2f94b1464eebe14170374
d.takeuchi <daiki.takeuchi.0327@gmail.com>
2016-12-07 00:28:29 +0900

    ルートの画面でHelloWorldが表示されるように修正

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -7,9 +7,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@RestController
 @EntityScan(basePackages = {"com.example.domain"})
 @EnableJpaRepositories(basePackages = {"com.example.domain"})
 @EnableTransactionManagement(proxyTargetClass = true)
@@ -18,6 +21,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+    @RequestMapping(value = "/")
+    String index() {
+        return "Hello World!";
+    }
 
 	//THYMELEAF Utility Object
 	@Bean
